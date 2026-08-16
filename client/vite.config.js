f6fbf7186396a9360cb9f3f09b55c8c27ba5e3de
suryavk31 <suryavk31@gmail.com>
2026-08-16 10:06:49 +0530

    Allow all hosts in client preview config

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
     strictPort: false,
+    allowedHosts: true,
   },
   build: {
     sourcemap: false,
